c51f62dbcf2b9e9edc7184113f15650aa8f7e880
parohodov@hotmail.com <parohodov@hotmail.com>
2022-06-02 15:12:00 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using HandyControl.Tools;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Bank
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ConfigHelper.Instance.SetLang("ru");
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+    }
+}
